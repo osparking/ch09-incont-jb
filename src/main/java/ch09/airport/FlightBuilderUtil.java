f64dd6bd3f9d77012df06d737b8e2c0941383dab
osparking <jbpark03@gmail.com>
2023-12-14 21:45:57 +0900

    비행편에 승객 정보 입력 유용 클래스

---
+++ src/main/java/ch09/airport/FlightBuilderUtil.java
@@ -0,0 +1,28 @@
+package ch09.airport;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FlightBuilderUtil {
+
+	public static Flight buildFlightFromCsv() throws IOException {
+		Flight flight = new Flight("AA1234", 20);
+
+		try (BufferedReader reader = new BufferedReader(
+				new FileReader("src/test/resources/flights_information.csv"))) {
+			String line = null;
+			do {
+				line = reader.readLine();
+				if (line != null) {
+					String[] passengerString = line.split(";");
+					Passenger passenger = new Passenger(passengerString[0].trim(),
+							passengerString[1].trim());
+					flight.addPassenger(passenger);
+				}
+			} while (line != null);
+		}
+
+		return flight;
+	}
+}
